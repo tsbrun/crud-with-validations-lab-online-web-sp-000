01afdd0e79ac39e1eac2feb825ff81506b2810f0
TSephanya Brun <tsbrun@icloud.com>
2020-12-01 13:53:56 -0500

    commit tree baa5afc4bff0c54c2a685d10778f8f7d6a588f98

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :songs, except: [:delete]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  delete '/songs/:id', :to => 'songs#destroy', :as => 'delete_song'
 end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Song.create(title: "Blank Space", released: true, release_year: 2016, artist_name: "Taylor Swift", genre: "pop")
+Song.create(title: "Love Story", released: true, release_year: 2012, artist_name: "Taylor Swift", genre: "country pop")
+Song.create(title: "Unreleased", released: false, artist_name: "Taylor Swift", genre: "unknown")
